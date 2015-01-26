d7b254d3ff5cbfd57bf5b3a44928d1903c72660d
Dargish <Dargish@gmail.com>
2015-01-26 09:20:51 +0000

    Updated build script to use VS120COMNTOOLS env variable

--- build.bat
+++ build.bat
@@ -4,4 +4,4 @@ rmdir _build /s /q
 mkdir _build
 cd _build
 cmake -DCMAKE_INSTALL_SUFFIX=./install ..
-"D:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" "%CD%\GLRender.sln"
+"%VS120COMNTOOLS%\..\IDE\devenv.exe" "%CD%\GLRender.sln"
