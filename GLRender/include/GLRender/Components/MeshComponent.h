48b5b34b56ea6bd5bb1e060e1c0327c522182653
Dargish <Dargish@gmail.com>
2015-02-18 00:30:02 +0000

    Mesh/Vertex/Index Buffer WIP

--- GLRender/include/GLRender/Components/MeshComponent.h
+++ GLRender/include/GLRender/Components/MeshComponent.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <GLRender/World/Component.h>
+#include <GLRender/Graphics/Mesh.h>
 
 namespace glr
 {
 	struct MeshComponent : public Component
 	{
+		std::shared_ptr<Mesh> mesh;
 	};
 }
---
+++ GLRender/include/GLRender/Graphics/Mesh.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <GLRender/DataTypes.h>
+
+namespace glr
+{
+	class Mesh
+	{
+	public:
+		
+	};
+}
